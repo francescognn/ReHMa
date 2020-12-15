883f76479a0106518180c833d0fb35af79ca669d
Francesco Gennari <francesco.gennari@fcagroup.com>
2020-12-15 17:26:22 +0100

    Add WIP comment

--- platform.py
+++ platform.py
@@ -15,7 +15,11 @@ while True:
                     print('0')
            sleep(1);           # Sleep for a full second before restarting our loop
 
-# Here we will need to decouple from the platform having: 
+# Io creerei un runner che abbia un Init, una step e una shutdown functions e la creerei come classe virtuale, per poi avere due classi che ereditano da questa: 
+    # - platform_runner (che userà librerie che possono girare solo su Raspberry per esempio GPIO) 
+    # - agnostic_runner (che farà le stesse cose ma con interfacce stubbate, dovremmo quindi creare un generatore di reader e un writer)
+# per quanto riguarda il testing io farei: 
+    # - unit tests (che testano le singole funzioni), ne ho creato uno di esempio
+    # - integration tests (test più completi che fanno girare tutto il codice simulando input e output
 
-# Hardware agnostic code
-# Hardware specific code
+# Il platform_runner girerà sulla Raspberry mentre il agnostic_runner girerà solo per poter fare gli integration tests stubbundo input e output
