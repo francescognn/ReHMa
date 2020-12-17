47f93252aed2e96be045f9e07d1c70053dddabf7
Francesco Gennari <francesco.gennari@fcagroup.com>
2020-12-17 23:23:19 +0100

    Add antifreeze mode

--- project/runner/runner.py
+++ project/runner/runner.py
@@ -10,6 +10,7 @@ class Runner:
         self.temperatures = {"Sala": 0.0}
         self.req_heater_state = False
         self.heater = Heater()
+        self.antifreeze_mode = False
 
     def init(self):
         print("Initializing")
@@ -17,10 +18,20 @@ class Runner:
     def step(self):
         self.read_temperatures()
         self.read_requests()
-
-        if self.req_heater_state != self.heater.get_status():
+        print("Temperature: ", self.temperatures["Sala"])
+        print("Heater initial status: ", self.heater.get_status())
+        if (self.req_heater_state != self.heater.get_status()):
             self.heater.set_status(self.req_heater_state)
-
+        
+        if (self.temperatures["Sala"] <= 5.0) :
+            self.heater.set_status(True)
+            self.antifreeze_mode = True
+        
+        if (self.antifreeze_mode) and (self.temperatures["Sala"] >= 7.0):
+            self.heater.set_status(False)
+            self.antifreeze_mode = False
+        print("Heater final status: ", self.heater.get_status())
+            
         self.publish_commands()
         self.upload_outputs()
 
