f96879e51136249ad584bbaf4bcde1db1bed0078
Francesco Gennari <francesco.gennari@fcagroup.com>
2020-12-17 23:24:45 +0100

    Remove comment

--- project/common/db_emulator.py
+++ project/common/db_emulator.py
@@ -4,5 +4,4 @@ import random
 
 class DbEmulator:
     def get_rand_heater_state(self):
-        # return bool(random.getrandbits(1))
-        return False
+        return bool(random.getrandbits(1))
