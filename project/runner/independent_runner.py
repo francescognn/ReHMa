a73c6e09dba49aae272592c68bc93d6f5a5d2be5
Francesco Gennari <francesco.gennari@fcagroup.com>
2020-12-21 11:34:23 +0100

    Add temperature publisher and subscriber

--- project/runner/independent_runner.py
+++ project/runner/independent_runner.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Float64
 from project.runner.runner import Runner
 
 
 class IndependentRunner(Runner):
     def __init__(self, IOs):
         Runner.__init__(self, IOs)
-        self.sub = rospy.Subscriber("heater_status", Bool, self.heater_status_callback)
+        self.sub_heater = rospy.Subscriber("heater_status", Bool, self.heater_status_callback)
+        self.sub_temperature = rospy.Subscriber("temperature", Float64, self.temperature_callback)
         rospy.init_node('Runner', anonymous=True)
         self.rate = rospy.Rate(10) # 10hz
         self.ros_heater_status = False
+        self.ros_temperatures = {"Sala": 0.0}
+    
+    def temperature_callback(self, data):
+        self.ros_temperatures["Sala"] = data
+        rospy.loginfo(data)    
 
     def heater_status_callback(self, data):
         self.current_heater_status = data
         rospy.loginfo(data)
         
     def read_temperatures(self):
-        self.temperatures["Sala"] = 10.0
+        self.temperatures = self.ros_temperatures 
 
     def read_heater_status(self):
         self.current_heater_status = self.ros_heater_status
--- project/test/utils/io_emulator.py
+++ project/test/utils/io_emulator.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 import random
 import rospy
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Float64
 
 global T_MIN
 global T_MAX
 global T_MAX_HEATER
 
 T_MIN = 2.0
-T_MAX = 20.0
-T_MAX_HEATER = 27.5
+T_MAX = 24.0
 
 
 class Emulator:
@@ -20,58 +19,41 @@ class Emulator:
         self.flag = True
         self.iterator = 0
         self.remote_request = False
-        self.pub = rospy.Publisher('heater_status', Bool, queue_size=10)
+        self.pub_heater_status = rospy.Publisher('heater_status', Bool, queue_size=10)
+        self.pub_temperature = rospy.Publisher('temperature', Float64, queue_size=10)
         rospy.init_node('emulator', anonymous=True)
         self.rate = rospy.Rate(10) # 10hz
 
     def step(self):
-        if self.config == "antifreeze":
-            if self.heater_status == True:
-                if self.current_temp < T_MAX_HEATER:
-                    self.current_temp += 0.1
-                if self.current_temp > 6.0:
-                    self.flag = False
-            if self.current_temp > T_MIN and self.flag:
-                self.current_temp -= 0.05
+        if self.config == "summer":
+            if self.current_temp < T_MAX:
+                self.current_temp += 0.05
 
         if self.config == "constant":
-            if self.heater_status == True:
-                if self.current_temp < T_MAX_HEATER:
-                    self.current_temp += 0.05
+            pass
 
-        if self.config == "trigger":
-            if self.heater_status == True:
-                if self.current_temp < T_MAX_HEATER:
-                    self.current_temp += 0.05
-            if self.iterator == 5:
-                self.remote_request = True
-            self.iterator += 1
-        
-        self.pub.publish(self.heater_status)
+        if self.config == "winter":
+            if self.current_temp > T_MIN:
+                self.current_temp -= 0.05
+
+        if self.heater_status == True and self.current_temp <= T_MAX:
+            self.current_temp += 0.1
+
+        self.pub_heater_status.publish(self.heater_status)
+        self.pub_temperature.publish(self.current_temp)
         self.rate.sleep()
-
-    def get_remote_request(self):
-        return self.remote_request
-
-    def get_temperature(self):
-        return self.current_temp
-
-    def get_heater_status(self):
-        return self.heater_status
-
-    def set_heater_status(self, status):
-        self.heater_status = status
 
     def set_config(self, config):
         self.config = config
-        if config == "antifreeze":
-            self.current_temp = 6.0
 
 if __name__ == '__main__':
     try:
         heater_emulator = Emulator()
+        heater_emulator.set_config("winter")
+
         while not rospy.is_shutdown():
-            heater_emulator.step()
+                heater_emulator.step()
+
     except rospy.ROSInterruptException:
         pass
         
